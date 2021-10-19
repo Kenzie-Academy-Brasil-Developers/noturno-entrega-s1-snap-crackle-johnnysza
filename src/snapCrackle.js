b17c5d3a8371bea92351de713d64e1ff781d1fe8
johnnysza <johnsilvasza@gmail.com>
2021-10-19 19:06:55 -0300

    Entrega Atividade 43

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,29 @@
 // Seu código vai aqui
+
+function snapCrackle(maxValue){
+    let array = []
+
+    for(let contador = 1; contador <= maxValue;contador++){
+        let num = ""
+        if(contador % 2 !== 0){
+            num += "Snap"
+            
+        }
+        if(contador % 5 === 0){
+            num += "Crackle"
+            
+        }
+        if(contador % 2 !== 0 && contador % 5 === 0){
+            num += ""
+            
+            
+        }
+        if(contador % 2 === 0 && contador % 5 !== 0){
+            num = contador
+            
+        }
+        array.push(num)
+    }
+    return array
+}
+console.log(snapCrackle())
